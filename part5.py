c5f70c99fd3dcc56c58a64727bcc1b0de3476956
pazitschrecker <38817948+pazitschrecker@users.noreply.github.com>
2018-11-27 18:21:28 -0500

    Update part5.py

--- part5.py
+++ part5.py
@@ -10,76 +10,67 @@
 # 5.2.  Calculate the proportion of primes that ends with 1 (1 is the right
 # digit)?  with 3?  with 7?  with 9?
 
-# Determines the last digit of a number
+# primes generated using sieve of Eratosthenes from Part 2
+# this generates the first 1.5 million prime numbers (23,879,519 is 1.5 millionth prime)
+primes = sieve(23879519) 
 
-def prop1():
-	primes = [2,3,5,7,11,13,17,19,23,29]
+def prop(n):
+	primes = sieve(23879519)
 
-	prime1 = []
+	primen = []
 	i = 0
 	
 	while i < len(primes):
-		if primes[i]%10 == 1:
-			prime1.append(primes[i])
+		if primes[i]%10 == n:
+			primen.append(primes[i])
 		i = i + 1
 
-	c = len(prime1)/len(primes)
-	print(c)
+	c = len(primen)/len(primes)
+	print(c, "of the first 1.5 million primes end in," n)
 
-prop1()
+prop(1)
+prop(3)
+prop(7)
+prop(9)
 
-def prop3():
-	primes = [2,3,5,7,11,13,17,19,23,29]
-
-	prime3 = []
+# 5.3 Calculates proportions of primes ending in some digit a followed by primes ending in some digit b
+def ends(a,b):     
+	primes = sieve(23879519)   
 	i = 0
-	
-	while i < len(primes):
-		if primes[i]%10 == 3:
-			prime3.append(primes[i])
+	j = 0  
+
+	while i < len(primes) - 1:
+		if primes[i]%10 == a and primes[i + 1]%10 == b:
+			j = j + 1
 		i = i + 1
 
-	c = len(prime3)/len(primes)
-	print(c)
+	d = j/len(primes)
+	print(d, "of the first 1.5 million primes ending with", a, "are followed by prime ending with", b)
 
-prop3()
+ends(1,1)
+ends(1,3)
+ends(1,7)
+ends(1,9)
 
-def prop7():
-	primes = [2,3,5,7,11,13,17,19,23,29]
+ends(3,1)
+ends(3,3)
+ends(3,7)
+ends(3,9)
 
-	prime7 = []
-	i = 0
-	
-	while i < len(primes):
-		if primes[i]%10 == 7:
-			prime7.append(primes[i])
-		i = i + 1
+ends(7,1)
+ends(7,3)
+ends(7,7)
+ends(7,9)
 
-	c = len(prime7)/len(primes)
-	print(c)
+ends(9,1)
+ends(9,3)
+ends(9,7)
+ends(9,9)
 
-prop7()
-
-def prop9():
-	primes = [2,3,5,7,11,13,17,19,23,29]
-
-	prime9 = []
-	i = 0
-	
-	while i < len(primes):
-		if primes[i]%10 == 9:
-			prime9.append(primes[i])
-		i = i + 1
-
-	c = len(prime9)/len(primes)
-	print(c)
-
-prop9()
 
 # determines number of twin prime pairs in list of generated primes
 def twins():
-	# primes generated using sieve of Eratosthenes from Part 2
-	primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]   
+	primes = sieve(23879519) 
 	i = 0
 	j = 0  
 
