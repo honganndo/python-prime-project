c26cbe8dff3b0fb4be44965aadadab931e6a4bef
pazitschrecker <38817948+pazitschrecker@users.noreply.github.com>
2018-11-27 19:06:43 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -22,38 +22,47 @@ def trial(n):
 		print(n, "is not prime \n")
 
 # 3.2: Using Sieve of Eratosthenes.
-def eratosthenes(n):
+
+# Helper function for eratosthenes that works like trial(n) but does not print anything
+def trial_erat(n):
 
 	a = 2
-	myList = []
+	b = math.ceil(math.sqrt(n))
 
-	while(a <= n):
-		myList.append(a)
-		a = a + 1
+	while a <= b:
+		if (n%a == 0):
+			a = b + 2
+		else:
+			a = a + 1
+
+	if a == b + 1:
+		return True
+	else:
+		return False
+
+def eratosthenes(n):
 
 	c = 2
-	d = 3
-	while (c < a):
-		
-		while(d < a):
-			if (d%c == 0) and d in myList:
-				myList.remove(d)
-			d = d + 1
+	d = math.ceil(math.sqrt(n))
+	myList = []
 
+	while(c <= d):
+		if (trial_erat(c) == True):
+			myList.append(c)
 		c = c + 1
-		d = c + 1
 
 	i = 0
 	while i < len(myList):
-		if n%myList[i] == 0 and n != myList[i]:
-			print(n, "is not prime")
-			i = len(myList) + 1
+		primey = myList[i]
+
+		if n%primey == 0:
+			print(n, "is not prime \n")
+			i = len(myList) + 20
 		else:
 			i = i + 1
 
-	if i == len(myList):
-		print(n, "is prime")
-		
+	if i <= len(myList):
+		print(n, "is prime \n")
 
 # 3.3: Fermat little theorem BONUS.
 def flt(p):
